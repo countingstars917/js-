55dd778f9d7a84a09699dfebf69413f9e9a6069f
lenovo_zhangjie <zhangctgu@gmail.com>
2025-07-22 20:17:03 +0800

    2024-7-22:提交七麦数据a参数js代码

---
+++ day6-摘要算法/1.七麦数据a参数.js
@@ -0,0 +1,35 @@
+//七麦数据a参数算法
+//函数定义
+function p(t) {
+                t = encodeURIComponent(t)["replace"](/%([0-9A-F]{2})/g, function(n, t) {
+                    return o("0x" + t)
+                });
+                try {
+                    return btoa(t)
+                } catch (n) {
+                    return R[W5][K5](t)[U5](Z5)
+                }
+            } //p方法
+
+function s(n) {
+                var n = new RegExp("(^| )" + "synct" + "=([^;]*)(;|$)");
+                return (n = document.cookie.match(n)) ? unescape(n[2]) : $
+            }; //s方法
+//参数定义
+var n;
+var a = [];
+var s = undefined = -s("syncd") || +new Date() - 1000 * n // s参数只有在页面刷新的时候根据算法生成
+var e, r = +new Date() - (s || 0) - 1661224081041
+
+
+//a参数算法
+Object.keys(t[Ot])["forEach"](function(n) {
+                        if (n == "analysis")
+                            return false;
+                        t[Ot]["hasOwnProperty"](n) && a["push"](t[Ot][n])})
+a = a["sort"]()["join"]("")
+a = p(a)
+a = (a += "@#" + "/rank/index"["replace"]("https://api.qimai.cn", "")) + ("@#" + r) + ("@#" + 3)
+
+
+//return只会返回最后一个，后的数据。逗号代表此行代码没有结束
